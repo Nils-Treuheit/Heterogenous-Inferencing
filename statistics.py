27cd24bffefcc2359f07e6267298bff3b29e807b
klemarthel <martin.mobile.2000@gmail.com>
2022-03-13 16:47:50 +0100

    distinction between the toolkits

--- statistics.py
+++ statistics.py
@@ -8,24 +8,28 @@ It's not really helpful to compute the min max values per iteration ...
 """
 
 for tk in csv_helpers.args_toolkits: #in targets unterscheiden
-    for net in common.tf_net_names:
-        path_net=Path(csv_helpers.getNetPath(tk,net))
-        files=path_net.glob("*single*")
-        measurements=[]
-        for file in files:
-            with open(file) as f:
-                rea=csv.reader(f)
-                next(rea) # remove titel
-                for x in rea:
-                    measurements.append(float(x[0]))
-                    #measurements
-                
+    for target in csv_helpers.args_target:
+        regex="*single*"+target+"*"
+        for net in common.tf_net_names:
+            path_net=Path(csv_helpers.getNetPath(tk,net))
+            files=path_net.glob(regex)
+            
+            measurements=[]
+            for file in files:
+                with open(file) as f:
+                    rea=csv.reader(f)
+                    next(rea) # remove titel
+                    for x in rea:
+                        measurements.append(float(x[0]))
+                        #measurements
 
-        m=np.array(measurements)
-        std=np.std(m)
-        avg=np.average(m)
-        min=np.min(m)
-        max=np.max(m)
+            if len(measurements)==0:
+                continue
+            m=np.array(measurements)
+            std=np.std(m)
+            avg=np.average(m)
+            min=np.min(m)
+            max=np.max(m)
 
-        print((net,std,avg,min,max))
+            print((net,std,avg,min,max,target))
         
